0a20e3933e57f7d3b51aace5ab0d81a89b0ad34d
Nick McMaster <31672832+ShirtMan@users.noreply.github.com>
2019-03-01 07:43:51 -0500

    Update Depot.java
    updated Depot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/Depot.java
@@ -9,18 +9,36 @@ import org.firstinspires.ftc.teamcode.core.util.ENUMS;
 @Autonomous
 public class Depot extends LinearOpMode {
     
-    private ENUMS.GoldLocation goldLocation = ENUMS.GoldLocation.UNKNOWN;
+     private ENUMS.GoldLocation goldLocation = ENUMS.GoldLocation.UNKNOWN;
 
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    private Robot robot;
+    
     private ENUMS.AutoStates robo = ENUMS.AutoStates.START;
 
-    private int sampleTurnDeg = 0;
-    private int wallTurnDeg = 45;
-    private int wallNavDist = 24;
+    private int sampleTurnDeg = 180;
+    private int sampleHitDist = -23;
+    private int sampleReturnDist = 10;
+    private int wallTurn1Deg = -90;
+    private int wallNav1Dist = -30;
+    private int wallTurn2Deg = 45;
+    private int wallNav2Dist = -14;
+    private int depotNavDist = 52;
+    private int craterNavDist = -75;
+
+    private boolean imuDone = false;
 
     @Override
     public void runOpMode() {
 
-        Robot robot = new Robot(hardwareMap, telemetry, Depot.this, true, true);
+        robot = new Robot(hardwareMap, telemetry, Crater.this, true, true);
 
         while(!isStopRequested() && !isStarted()) {
 
@@ -28,23 +46,33 @@ public class Depot extends LinearOpMode {
 
             switch(goldLocation){
                 case LEFT:{
-                    sampleTurnDeg = 30;
+                    sampleTurnDeg = 225;
+                    wallNav1Dist -= 14.5;
+                    sampleHitDist = -27;
+                    sampleReturnDist = 12;
+                    //wallTurn1Deg = 45;
                     break;
                 }
                 case CENTER:{
-                    sampleTurnDeg = 0;
-                    wallNavDist += 14.5;
+                    sampleTurnDeg = 180;
                     break;
                 }
                 case RIGHT:{
-                    sampleTurnDeg = -30;
-                    wallNavDist += (14.5*2);
+                    sampleTurnDeg = 135;
+                    wallNav1Dist += 14.5;
+                    sampleHitDist = -27;
+                    sampleReturnDist = 12;
+                    //wallTurn1Deg = 45 + 90;
                 }
             }
 
+            robot.tm.setTMDown();
 
             telemetry.update();
         }
+
+        robot.drive.initIMU();
+        imuDone = true;
 
         while(!isStopRequested() && opModeIsActive()){
 
@@ -59,9 +87,10 @@ public class Depot extends LinearOpMode {
 
                     robot.hanger.moveToGround();
 
-                    robot.drive.initIMU();
+                    telemetry.addData("heading:", robot.drive.getAngle());
+                    telemetry.update();
 
-                    robot.drive.rotate(180);
+                    robot.drive.turnAbsoulte(sampleTurnDeg);
 
                     robo = ENUMS.AutoStates.HITGOLD;
                     break;
@@ -69,23 +98,69 @@ public class Depot extends LinearOpMode {
 
                 case HITGOLD:{
 
-                    robot.drive.rotate(sampleTurnDeg);
-                    robot.drive.encoderDrive(0.6, 24, 4.0);
-                    sleep(300);
-                    robot.drive.encoderDrive(0.6, -24, 4.0);
+                    robot.drive.drive(sampleHitDist);
+                    sleep(200);
+                    robot.drive.drive(sampleReturnDist);
+                    sleep(200);
 
                     robo = ENUMS.AutoStates.NAVTOWALL;
                     break;
                 }
 
-                case NAVTOWALL:{
+                case NAVTOWALL: {
 
+                    robot.drive.turnAbsoulte(wallTurn1Deg);
+                    sleep(200);
+                    robot.drive.drive(wallNav1Dist);
+                    robot.drive.turnAbsoulte(wallTurn2Deg); //facing depot while parallel to wall
+                    sleep(200);
+                    robot.drive.drive(wallNav2Dist);
+                    sleep(200);
 
-                    robot.drive.rotate(wallTurnDeg);
-                    robot.drive.encoderDrive(0.6, wallNavDist, 10.0);
-                    robot.drive.rotate(45); //facing depot while parallel to wall
+                    robo = ENUMS.AutoStates.ALIGNWITHWALL;
+                    break;
+                }
 
+                case ALIGNWITHWALL:{
 
+                    robot.drive.turnAbsoulte(135);
+                    sleep(200);
+                    robot.drive.setStrafePower(0.6);
+                    sleep(200);
+                    robot.drive.setStrafePower(-0.2);
+                    sleep(200);
+                    robot.drive.setStrafePower(0);
+                    sleep(200);
+
+                    robo = ENUMS.AutoStates.NAVTOTM;
+                    break;
+
+                }
+
+                case NAVTOTM:{
+
+                    robot.drive.drive(depotNavDist);
+                    sleep(200);
+
+                    robo = ENUMS.AutoStates.DROPTM;
+                    break;
+                }
+
+                case DROPTM: {
+
+                    robot.tm.setTMUp();
+                    sleep(500);
+
+                    robo = ENUMS.AutoStates.PARK;
+                    break;
+                }
+
+                case PARK: {
+
+                    robot.drive.drive(craterNavDist); // approx.
+
+                    robo = ENUMS.AutoStates.END;
+                    break;
                 }
 
                 case END:{
@@ -96,6 +171,13 @@ public class Depot extends LinearOpMode {
 
             }
 
+            robot.tm.setTMDown();
+
+            idle();
+            
+            if(imuDone){
+                telemetry.addData("heading:", robot.drive.getAngle());
+            }
             telemetry.addLine("state: " + robo);
             telemetry.update();
         }
